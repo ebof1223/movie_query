16c45a8d9ccd22b3a12854c480f795afec4b9f7f
ebof1223 <rmf97@txstate.edu>
2025-05-11 15:17:46 -0500

    met requirements

---
+++ README.md
@@ -0,0 +1,12 @@
+## Data Input
+  Took input and validated with input_hannder such that
+    1. Its a csv file
+    2. a valid path was provided
+
+## Query Options
+  1. added flags via argparse
+  2. flags are optional by default and if provided have type safety so validation should be good
+
+## Output
+  1.std out headers 
+  2.std out each row each that meets input constraints
--- movie_query.py
+++ movie_query.py
@@ -13,51 +13,47 @@ def input_handler(path:str)->bool:
 
     return True
 
-def main():
+def main() -> int:
     parser = argparse.ArgumentParser()
-    #TODO: validate flags
     parser.add_argument('--input', type=str)
     parser.add_argument('--year-after', type=str)
     parser.add_argument('--genre', type=str)
-    parser.add_argument('--rating-above', type=int)
+    parser.add_argument('--rating-above', type=float)
     parser.add_argument('--director', type=str)
     parser.add_argument('--actor', type=str)
     parser.add_argument('--runtime-more-than', type=str)
     parser.add_argument('--gross-min', type=int)
+    parser.add_argument('--output-format', type=str)
+    parser.add_argument('--output-file', type=str)
 
     args = parser.parse_args()
     path = args.input or ""
-    input_handler(path)
-    
+
+    if not input_handler(path):
+        return 1
+
     with open(path) as imdb_top_1000:
         reader = csv.DictReader(imdb_top_1000)
         sorted_by_rating = sorted(
             list(reader), key=lambda h:float(h["imdb_rating"]), reverse=True
         )
+        print(reader.fieldnames)
         for item in sorted_by_rating:
-            if args.year_after:
-                if not item["released_year"].isdigit() or item["released_year"] <= args.year_after:
+            if args.year_after and (not item["released_year"].isdigit() or item["released_year"] <= args.year_after):
+                continue
+            if args.genre and args.genre not in item["genre"]:
                     continue
-            if args.genre:
-                if args.genre not in item["genre"]:
+            if args.rating_above and float(item["imdb_rating"])<= args.rating_above:
                     continue
-            if args.rating_above:
-                if float(item["imdb_rating"])<= args.rating_above:
+            if args.director and args.director != item["director"]:
                     continue
-            if args.director:
-                if item["director"] is not args.director:
+            if args.actor and args.actor not in [item["star_1"],item["star_2"], item["star_3"], item["star_4"] ]:
+                continue
+            if args.runtime_more_than and int(item["runtime"].replace(" min", "")) <= args.runtime_more_than:
                     continue
-            if args.actor:
-                if args.actor not in [
-                    item["star_1"],item["star_2"], item["star_3"], item["star_4"]
-                ]:
-                    continue
-            if args.runtime_more_than:
-                if int(item["runtime"].replace(" min", "")) <= args.runtime_more_than:
-                    continue
-            if args.gross_min:
-                if item["gross"] or 0 <= args.runtime_more_than:
-                    continue
-            print(item)
+            if args.gross_min and item["gross"].isdigit() and int(item["gross"]) <= args.gross_min:
+                continue
+            print(list(item.values()))
+    return 0
 
 main()
