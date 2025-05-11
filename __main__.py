b7b8a5e82f132a366dcc86dcaa8454e67af328a0
ebof1223 <rmf97@txstate.edu>
2025-05-11 13:32:29 -0500

    flag based input

--- __main__.py
+++ __main__.py
@@ -1,28 +1,23 @@
-import csv
+import argparse
 
-def input_hanlder(path:str)->bool:
+def input_handler(path:str)->bool:
     try:
         with open(path) as _:
             if path[-4:] != ".csv":
-                print("Invalid file. Provide valid file.\n")
+                print("Invalid input flag. Provide valid file.")
                 return False
     except FileNotFoundError:
-        print("Invalid path. Provide valid path.\n")
+        print("Invalid input flat. Provide valid path.")
         return False
 
     return True
 
 def main():
-    # path = input("Enter path for csv to input: ")
-    path = "./imdb_top_1000.csv"
-    while not input_hanlder(path):
-        path = input("Enter path for csv to input: ")
-
-
-    with open(path) as idmb_top_1000:
-        csv_reader = csv.DictReader(idmb_top_1000)
-        for line in csv_reader:
-            print(line)
+    parser =argparse.ArgumentParser()
+    parser.add_argument('-i','--input', type=str, help="Pass input path as flag")
+    args = parser.parse_args()
+    path = args.input
+    input_handler(path)
 
 
 if __name__ == "__main__":
